aeb0ad36d7c7f954600fa861a2566a1a83e16fb3
Your Name <ishmamp3@gmail.com>
2024-12-31 19:52:20 -0800

    full page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
 import Image from "next/image";
 import Hero from "@/components/Hero";
+import Statistics from "@/components/Statistics";
+import Services from "@/components/Services";
+import Testimonial from "@/components/Testimonial";
+import Footer from "@/components/Footer";
+
 
 export default function Home() {
   return (
-    <Hero/>
+    <main className="min-h-screen flex flex-col">
+      <Hero />
+      <Statistics />
+      <Services />
+      <Testimonial />
+      <Footer />
+    </main>
   );
 }
